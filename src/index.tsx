e37ad5af1f2405edfdd21c6ea59e0a8f7fc54349
João Mantovani <jpm.mantovani45@gmail.com>
2022-05-01 22:13:17 -0300

    chore: migrate to mui 5

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 // import { AuthProvider } from './contexts/JWTContext';
 
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById('root'),
-);
+// ReactDOM.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+//   document.getElementById('root'),
+// );
+
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
+
